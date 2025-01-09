a01f5e5c11a0130658fc0168e8550fd74fd7e733
Diogo Ferreira <itsnotsoftware@gmail.com>
2025-01-09 13:18:44 +0000

    automatic display assignement

---
+++ i3/.config/i3/scripts/monitor_setup.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Detect monitors dynamically
+INTERNAL=$(xrandr | grep "eDP" | grep " connected" | awk '{ print $1 }')
+EXTERNAL=$(xrandr | grep "HDMI" | grep " connected" | awk '{ print $1 }')
+
+# Apply layout
+if [[ -n "$EXTERNAL" && -n "$INTERNAL" ]]; then
+  # External and internal monitors connected
+  xrandr --output "$INTERNAL" --primary --auto --pos 0x0 \
+         --output "$EXTERNAL" --auto --left-of "$INTERNAL"
+elif [[ -n "$EXTERNAL" ]]; then
+  # Only external monitor connected
+  xrandr --output "$EXTERNAL" --primary --auto \
+         --output "$INTERNAL" --off
+elif [[ -n "$INTERNAL" ]]; then
+  # Only internal monitor connected
+  xrandr --output "$INTERNAL" --primary --auto \
+         --output "$EXTERNAL" --off
+else
+  echo "No connected monitors found."
+fi
+
